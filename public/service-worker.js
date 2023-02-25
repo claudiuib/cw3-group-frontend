95a118f1e56ab73dd7cb33f0019e0b4375679f8a
claudiu <busteagaclaudiu@gmail.com>
2023-02-25 23:49:42 +0000

    update webmanifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,28 @@ self.addEventListener("install",function(e){
    
    });
 
-   self.addEventListener("fetch",function(e){
+//    self.addEventListener("fetch",function(e){
+//     e.respondWith(
+//         caches.match(e.request).then(function(cachedFiles){
+//             if(cachedFiles){
+//                 console.log("{Service Worker}Resource fetched from the cache for: "
+//                 + e.request.url);
+//                 return cachedFiles
+//             } else{
+//                 return fetch (e.request).then(function(response){
+
+//                     return caches.open(cacheName).then(function(cache){
+//                         cache.put(e.request,  response.clone());
+//                         console.log("{Service Work} resource fetch  and saved in the cache :"
+//                         + e.request.url);
+//                         return response;
+//                     });
+//                 });
+//             }
+//         })
+//     )
+//    })
+self.addEventListener("fetch",function(e){
     e.respondWith(
         caches.match(e.request).then(function(cachedFiles){
             if(cachedFiles){
@@ -26,13 +47,15 @@ self.addEventListener("install",function(e){
                 return cachedFiles
             } else{
                 return fetch (e.request).then(function(response){
-
-                    return caches.open(cacheName).then(function(cache){
-                        cache.put(e.request,  response.clone());
-                        console.log("{Service Work} resource fetch  and saved in the cache :"
-                        + e.request.url);
-                        return response;
-                    });
+                    let validUrl = new URL(e.request.url).protocol === "http:" || new URL(e.request.url).protocol === "https:";
+                    if(validUrl){
+                        return caches.open(cacheName).then(function(cache){
+                            cache.put(e.request,  response.clone());
+                            console.log("{Service Work} resource fetch  and saved in the cache :"
+                            + e.request.url);
+                            return response;
+                        });
+                    }
                 });
             }
         })
