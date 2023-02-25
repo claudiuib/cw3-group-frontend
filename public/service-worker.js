980285fead37f267b1c928112a505ffe16e8f8d3
claudiu <busteagaclaudiu@gmail.com>
2023-02-25 23:12:22 +0000

    update webmanifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,3 +16,24 @@ self.addEventListener("install",function(e){
     );
    
    });
+
+   self.addEventListener("fetch",function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(cachedFiles){
+            if(cachedFiles){
+                console.log("{Service Worker}Resource fetched from the cache for: "
+                + e.request.url);
+                return cachedFiles
+            } else{
+                return fetch (e.request).then(function(response){
+                    return caches.open(cacheName).then(function(cache){
+                        cache.put(e.request,response.clone());
+                        console.log("{Service Work} resource fetch  and saved in the cache :"
+                        +e.request.url);
+                        return response;
+                    });
+                });
+            }
+        })
+    )
+   })
