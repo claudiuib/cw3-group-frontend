97a14252962163c1cd38981830f632fc76836bac
claudiu <busteagaclaudiu@gmail.com>
2023-02-26 00:16:04 +0000

    update webmanifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,26 +17,6 @@ self.addEventListener("install",function(e){
    
    });
 
-//    self.addEventListener('fetch', function (e) {
-//     e.respondWith(
-//         caches.match(e.request).then(function (cachedFiles) {
-//             if (cachedFiles) {
-//                 console.log(`{Service Worker} Resource fetched from the cache for: ${e.request.url}`);
-//                 return cachedFiles;
-//             } else {
-//                 return fetch(e.request).then(function (response) {
-//                     if (response.url.startsWith('https')) {
-//                         return caches.open(cacheName).then(function (cache) {
-//                             cache.put(e.request, response.clone());
-//                             console.log(`{Service Work} resource fetch  and saved in the cache : ${e.request.url}`);
-//                             return response;
-//                         });
-//                     }
-//                 });
-//             }
-//         })
-//     );
-// });
    self.addEventListener("fetch",function(e){
     e.respondWith(
         caches.match(e.request).then(function(cachedFiles){
@@ -45,7 +25,7 @@ self.addEventListener("install",function(e){
                 + e.request.url);
                 return cachedFiles
             } else{
-                return fetch (e.request).then(function(response){
+                return fetch(e.request).then(function(response){
 
                     return caches.open(cacheName).then(function(cache){
                         cache.put(e.request,  response.clone());
